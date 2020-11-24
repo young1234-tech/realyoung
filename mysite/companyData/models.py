62dc6bbfd8e660b27dc2d4c7bcce07df89b1d126
최영진 <samkim000036@gmail.com>
2020-11-24 17:01:48 +0000

    7

--- mysite/companyData/models.py
+++ mysite/companyData/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
 class CompanyData(models.Model):
     companyName = models.CharField(max_length=200)
     stockCode = models.CharField(max_length=200)
     companyBuz = models.TextField()
     companyCEO = models.CharField(max_length=200)
-    
+
     def __str__(self):
         return self.companyName
