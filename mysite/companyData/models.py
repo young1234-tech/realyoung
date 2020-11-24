907d034d9bd6813d6b7cf6a7119261c23d06250b
최영진 <samkim000036@gmail.com>
2020-11-24 17:10:16 +0000

    8

--- mysite/companyData/models.py
+++ mysite/companyData/models.py
@@ -5,7 +5,11 @@ class CompanyData(models.Model):
     companyName = models.CharField(max_length=200)
     stockCode = models.CharField(max_length=200)
     companyBuz = models.TextField()
+    companyProd = models.TextField()
+    companyListStartDate = models.CharField(max_length=200)
     companyCEO = models.CharField(max_length=200)
+    companySite = models.CharField(max_length=200)
+    companyLocal = models.CharField(max_length=200)
 
     def __str__(self):
         return self.companyName
